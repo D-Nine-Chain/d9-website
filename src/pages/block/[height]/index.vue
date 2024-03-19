9ce0d6c36f2453b419fb380f2a012bf914f9ff6a
Cyenoch <cyenoch@outlook.com>
2024-03-19 18:47:24 +0800

    update

--- src/pages/block/[height]/index.vue
+++ src/pages/block/[height]/index.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+// import { useToast } from 'primevue/usetoast'
 import { useBalancesTransferEvents } from '~/composables/extrinsics/balances'
 
+// const toast = useToast()
 const route = useRoute('/block/[height]/')
 
 const { height } = route.params
 
-const { state: block } = await useBlock(height)
+const { state: block, error } = await useBlock(height)
 
 const transfers = useBalancesTransferEvents(block)
+
+watch(error, (err) => {
+  // err && toast.add({ severity: 'error', summary: 'Error', detail: err?.toString(), life: 3500 })
+  err && console.warn(err)
+})
 </script>
 
 <template>
